8254e1b90838b38fc38c535b2311d814460becdd
seoyoung81 <seosue0801@gmail.com>
2024-01-09 16:24:30 +0900

    feature: 2번째 header (검색창 제외)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 
+// 모든 페이지에 필요한 번개장터 Header2
+import Header2 from '@/components/main/Header2'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -16,7 +19,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header2></Header2>
+        {children}
+        </body>
     </html>
   )
 }
