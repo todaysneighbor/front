aef3193b16e09784d6d335ef30f55eed49be14bb
seoyoung81 <seosue0801@gmail.com>
2024-01-23 00:04:43 +0900

    feat: 모달 창 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,14 @@ import SignUp from '@/components/main/molecules/SignUp'
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between">
+
+      {/* 캐러셀 배너 */}
       <MainBanner></MainBanner>
+
+      {/* 앱 다운로드 배너 */}
       <AppDownLoadBanner></AppDownLoadBanner>
+      
+      {/* 로그인 모달창 */}
       <SignUp></SignUp>
     </main>
   )
--- src/components/main/molecules/NavRightLoggedOut.tsx
+++ src/components/main/molecules/NavRightLoggedOut.tsx
@@ -1,10 +1,16 @@
+'use client'
+
+import useModalStore from "@/zustand/LogInModalStore"
+
 // 로그아웃 일 때의 컴포넌트
 export default function NavRightLoggedOut () {
+    // 로그아웃일 때 로그인 모달 창 오픈 버튼이 되어줘야 한다
+    const { openModal } = useModalStore();
 
     return (
         <div className="flex items-center">
-            <div className="text-[13px] font-[#666666] px-[15px]">로그인/회원가입</div>
-            <div className="text-[13px] font-[#666666] px-[15px]">내상점</div>
+            <div className="text-[13px] font-[#666666] px-[15px]" onClick={openModal}>로그인/회원가입</div>
+            <div className="text-[13px] font-[#666666] px-[15px]" onClick={openModal}>내상점</div>
         </div>
     )
 }
