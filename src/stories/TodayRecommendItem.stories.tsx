acc2b159cb6f46e2c2ed64d7ffddd6f23c5f89c3
seoyoung81 <seosue0801@gmail.com>
2024-01-27 00:03:06 +0900

    fix: run build type 문제 해결 및 storybook type 문제 해결

--- src/stories/TodayRecommendItem.stories.tsx
+++ src/stories/TodayRecommendItem.stories.tsx
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 
 import TodayRecommendItem from './TodayRecommendItem';
 
-export default {
+const meta =  {
     title: 'Item/TodayRecommend',
     component: TodayRecommendItem,
     // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
@@ -12,30 +12,33 @@ export default {
     },
     argTypes: {
 
-
     }
+} satisfies Meta<typeof TodayRecommendItem>
 
-} 
+export default meta;
 
-const Template = (args: any) => <TodayRecommendItem {...args} />;
-
-export const Default = Template.bind({});
-export const LongItem = Template.bind({});
+// 스토리
+type Story = StoryObj<typeof meta>;
 
 // 기본형
-Default.args = {
+export const Default = {
+  args: {
     itemId: 1,
     imgSrc: 'https://media.bunjang.co.kr/product/250825353_1_1706145841_w292.jpg',
     itemName: '멋진 구두',
     itemPrice: 30000,
     itemDate: new Date(2024, 0, 25, 12, 0)
+
+  }
 }
 
 // 이름이 긴 상품
-LongItem.args = {
-  itemId: 2,
-  imgSrc: 'https://media.bunjang.co.kr/product/250825353_1_1706145841_w292.jpg',
-  itemName: '이름이 엄청 긴 멋진 검정 구두 입니다. 잘 팔렸으면 좋겠다.',
-  itemPrice: 35000,
-  itemDate: new Date(2024, 0, 20, 12, 0)
+export const LongItem = {
+  args: {
+    itemId: 2,
+    imgSrc: 'https://media.bunjang.co.kr/product/250825353_1_1706145841_w292.jpg',
+    itemName: '이름이 엄청 긴 멋진 검정 구두 입니다. 잘 팔렸으면 좋겠다.',
+    itemPrice: 35000,
+    itemDate: new Date(2024, 0, 26, 12, 0)  
+  }
 }
