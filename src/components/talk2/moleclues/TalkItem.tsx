dd534902f0ded0eca87b9acb8383fd5205bbe41f
s01k1m <sk618dev@gmail.com>
2024-03-01 23:39:05 +0900

    feat: 채팅 영역

--- src/components/talk2/moleclues/TalkItem.tsx
+++ src/components/talk2/moleclues/TalkItem.tsx
@@ -1,7 +1,49 @@
+import Image from 'next/image'
+import { CiShop } from 'react-icons/ci'
+
+import { format } from 'date-fns'
+import { ko } from 'date-fns/locale'
+
 export default function TalkItem() {
+  function findDate(year: string, month: string, day: string) {
+    const result = format(
+      new (Date as any)(year, month, day),
+      'yyyy년 MM월 dd일',
+    )
+    // 주의 : month 인덱스 0부터 시작함
+    return result
+  }
+
   return (
     <div>
-      <div></div>
+      <div className="grid place-items-center content-center my-2 p-3">
+        {/* 상점소개 */}
+        <div className="w-[100px] h-[100px] relative cursor-pointer ">
+          {/* TODO: 링크 추가해야함 */}
+          <CiShop
+            className="rounded-full inline-block bg-gray-300 w-full h-full p-3"
+            color="white"
+          />
+
+          {/* 이미지 주소가 있다면
+          <Image
+            fill
+            src="{'이미지 주소'}"
+            alt="상점 프로필 이미지"
+            className="rounded-full"
+          ></Image> */}
+        </div>
+        <div id="storeName" className="font-bold m-2">
+          상점이름
+        </div>
+        {/* 채팅 시작 날짜 */}
+        <div className="grid grid-cols-5 w-full h-[20px] text-[10px] text-border-gray items-center">
+          <hr className="w-full col-span-2 bg-border-gray" />
+          <span className="text-center"> {findDate('2023', '1', '11')}</span>
+          <hr className="w-full col-span-2 bg-border-gray" />
+        </div>
+      </div>
+      {/* 채팅 말풍선 시작 */}
     </div>
   )
 }
--- src/components/talk2/organisms/TalkItemList.tsx
+++ src/components/talk2/organisms/TalkItemList.tsx
@@ -1,7 +1,9 @@
 'use client'
 
 import { useState } from 'react'
+import Image from 'next/image'
 import { FaArrowCircleUp } from 'react-icons/fa'
+import TalkItem from '../moleclues/TalkItem'
 
 export interface TalkItem {
   StoreName: string
@@ -9,38 +11,59 @@ export interface TalkItem {
 }
 
 export default function TalkItemList(props: { props: any }) {
-  const [sendMessage, setSendMessage] = useState<string>('')
+  const [message, setMessage] = useState<string>('')
   // 키 enter 메시지 보내는 이벤트 처리
   const handleKeyPress = (e: KeyboardEvent) => {
     if (e.key == ' ' || e.code == 'Space' || e.keyCode == 32) {
-      handleClick()
+      sendMessage
       const target = e.target as HTMLInputElement
       target.blur() // placehorder 안나오는 문제 해결함 : 포커스 해제
     }
   }
-  const handleClick = () => {
+  const sendMessage = (event: any) => {
+    event.preventDefault
+
     // 메세지 보내고
     // 지우기
-    // setSendMessage('')
+    // setMessage('')
   }
   return (
     <div>
-      <div id="talkHeader">상점 이름 Link</div>
+      <div
+        id="talkProduct"
+        className="pt-2 grid-cols-8 grid justify-items-center"
+      >
+        <div className="w-[50px] h-[50px] relative ">
+          <Image
+            fill
+            src="https://i.ebayimg.com/images/g/r4UAAOSwgQhgsMhT/s-l1600.jpg"
+            alt="상점 프로필 이미지"
+            className="rounded-sm"
+          ></Image>
+        </div>
+        <div className="col-span-6 w-full flex flex-col text-border-gray text-[12px] justify-center">
+          <span className="text-black font-bold">상품가격(원)</span>
+          <span>상품이름</span>
+        </div>
+      </div>
+      <hr className="my-2 w-full"></hr>
       <div>
-        채팅 리스트
-        <div>스토어 이름</div>
-        <div>날짜</div>
-        <div>채팅 시작</div>
+        {/* 채팅 기록 */}
+        <TalkItem />
+        {/* 채팅 입력 */}
         <form className="relative p-2">
           <input
             type="text"
             className=" w-full h-[50px] rounded-full bg-hover-gray focus:outline-none px-5 pe-12 overflow-hidden"
             placeholder="메시지를 입력하세요."
-            onChange={(e) => setSendMessage(e.target.value)}
+            onChange={(e) => setMessage(e.target.value)}
             onKeyDown={(e) => handleKeyPress(e)}
           ></input>
-          {sendMessage ? (
-            <button className="absolute right-0 me-5 m-3 w-[25px] h-[25px]">
+          {message ? (
+            <button
+              className="absolute right-0 me-5 m-3 w-[25px] h-[25px]"
+              onClick={sendMessage}
+            >
               <FaArrowCircleUp color="red" size="26" />
             </button>
           ) : null}
