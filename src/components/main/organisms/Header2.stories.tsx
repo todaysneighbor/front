a7d70c0553d993b5c926f89f76c330ac6a2379ad
seoyoung81 <seosue0801@gmail.com>
2024-03-08 15:07:35 +0900

    feat: 정적 이미지 업로드 위한 설정 추가

--- src/components/main/organisms/Header2.stories.tsx
+++ src/components/main/organisms/Header2.stories.tsx
@@ -15,6 +15,10 @@ const meta = {
 
 export default meta
 
+export const Image = {
+  render: () => <img src="/product sell logo.png" alt="img 어디 진짜" />
+}
+
 export function Search() {
   return <SearchInput />
 }
@@ -22,3 +26,10 @@ export function Search() {
 export function Menu() {
   return <MenuButton />
 }
+
+export const Default = {
+  args: {
+
+  }
+}
+
